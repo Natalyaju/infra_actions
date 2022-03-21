40053ad1d5b681f152eaee820c3a432603c7b012
Natalyaju <fibi_fruits@mail.ru>
2022-03-21 22:38:45 +0300

    V10

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 
 
-def index(request, Exception=None):
+def index(request, exception=None):
     return HttpResponse('У меня получилось!', status=200)
 
 
-def second_page(request, Exception=None):
+def second_page(request, exception=None):
     return HttpResponse('А это вторая страница', status=200)
